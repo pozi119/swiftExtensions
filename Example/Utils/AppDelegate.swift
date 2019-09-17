4cfbd9bc86a0c1a16ddee17f39adacad04c6d2dd
Li Jinbo <pozi119@163.com>
2019-09-17 13:44:52 +0800

    Initial commit

---
+++ Example/Utils/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Utils
+//
+//  Created by pozi119 on 09/17/2019.
+//  Copyright (c) 2019 pozi119. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/Utils/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Utils
+//
+//  Created by pozi119 on 09/17/2019.
+//  Copyright (c) 2019 pozi119. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
